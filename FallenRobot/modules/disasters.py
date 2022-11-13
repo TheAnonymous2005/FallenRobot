eb2998b57f0ad9b72cf38cf3203f0e96a54a71ec
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2022-11-13 14:29:11 +0530

    ɴᴛɢ

--- FallenRobot/modules/disasters.py
+++ FallenRobot/modules/disasters.py
@@ -679,7 +679,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Devs​"
+__mod_name__ = "Dᴇᴠs "
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
